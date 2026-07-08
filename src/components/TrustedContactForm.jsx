498f0952abaa16920ce5f5edd091245c655cbdba
Munnisha <munnishav9@gmail.com>
2026-07-08 18:53:11 -0500

    I added input fields for contact form.

--- src/components/TrustedContactForm.jsx
+++ src/components/TrustedContactForm.jsx
@@ -6,7 +6,76 @@ function TrustedContactForm(){
     return(
 
         <div>
-            <h3> contacts: </h3>
+            <h3> contacts: </h3> <br />
+            <form>
+              <h4> Contact 1</h4>
+
+                <label> Name </label>
+                <input type='text' name='name' value = { name} /> <br />
+
+                <label> Mobile Number </label>
+                <input type ="tel" id='mobileNumber' name='mobileNumber' value ={mobileNumber}/> <br />
+
+                <lable> Relationship </lable>
+                <input type='text' name='relationship' value = { relationship} /> <br />
+
+                <button type='submit'> Save </button>
+
+              <h4> Contact 2</h4>
+
+                <label> Name </label>
+                <input type='text' name='name' value = { name} /> <br />
+
+                <label> Mobile Number </label>
+                <input type ="tel" id='mobileNumber' name='mobileNumber' value ={mobileNumber}/> <br />
+
+                <lable> Relationship </lable>
+                <input type='text' name='relationship' value = { relationship} /> <br />
+
+                <button type='submit'> Save </button>
+
+              <h4> Contact 3 </h4>
+
+                <label> Name </label>
+                <input type='text' name='name' value = { name} /> <br />
+
+                <label> Mobile Number </label>
+                <input type ="tel" id='mobileNumber' name='mobileNumber' value ={mobileNumber}/> <br />
+
+                <lable> Relationship </lable>
+                <input type='text' name='relationship' value = { relationship} /> <br />
+
+                <button type='submit'> Save </button>
+
+            <h4> Contact 4 </h4>
+
+                <label> Name </label>
+                <input type='text' name='name' value = { name} /> <br />
+
+                <label> Mobile Number </label>
+                <input type ="tel" id='mobileNumber' name='mobileNumber' value ={mobileNumber}/> <br />
+
+                <lable> Relationship </lable>
+                <input type='text' name='relationship' value = { relationship} /> <br />
+
+                <button type='submit'> Save </button>
+
+            <h4> Contact 5 </h4>
+
+                <label> Name </label>
+                <input type='text' name='name' value = { name} /> <br />
+
+                <label> Mobile Number </label>
+                <input type ="tel" id='mobileNumber' name='mobileNumber' value ={mobileNumber}/> <br />
+
+                <lable> Relationship </lable>
+                <input type='text' name='relationship' value = { relationship} /> <br />
+
+                <button type='submit'> Save </button>
+
+            </form>
         </div>
     );
 }
+
+export default TrustedContactForm;
