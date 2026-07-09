33024a39594d82c93540947d0f009712788dece4
Munnisha <munnishav9@gmail.com>
2026-07-09 10:23:22 -0500

    I Added one click send message options Using Buttons.

--- src/pages/OneClickSendMessage.jsx
+++ src/pages/OneClickSendMessage.jsx
@@ -1,11 +1,19 @@
 
-
+import react from 'react';
 
 function OneClickSendMessage(){
 
     return( 
-          
-        <h4> Send a Quick Message </h4>
+       <div>   
+           <h4> Send a Quick Message </h4>
+           <button> I am at Home.</button>
+           <button> I am travelling right now. I will update you when I reach my destination.</button>
+           <button>Everything is okay. I am safe. </button>
+           <button> I need help. Please contact me as soon as possible. </button>
+           <button> I am at the office </button>
+           <button> Emergency </button>
+           <button> I am at the park. </button>
+       </div>
     );
 }
 
