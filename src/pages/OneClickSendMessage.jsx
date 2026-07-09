52b1668f4267d0b0ef8725f660a1b64d37225e8e
Munnisha <munnishav9@gmail.com>
2026-07-09 12:22:41 -0500

    Created one click message component using state, .map function, and event handlers for buttons.

--- src/pages/OneClickSendMessage.jsx
+++ src/pages/OneClickSendMessage.jsx
@@ -1,18 +1,36 @@
 
-import react from 'react';
+import React, {useState} from 'react';
 
 function OneClickSendMessage(){
+    const [ message, setMessage] = useState('');
+
+    const messages = [
+
+        "I am at Home.", 
+        " I am travelling right now. I will update you when I reach my destination. ",
+        " Everything is okay. I am safe. ",
+        " I need help. Please contact me as soon as possible.",
+        " I am at the office",
+        " Emergency ",
+        " I am at the park."
+
+    ];
+      
+    const sendMessageHandler = () => { };
 
     return( 
        <div>   
            <h4> Send a Quick Message </h4>
-           <button> I am at Home.</button>
-           <button> I am travelling right now. I will update you when I reach my destination.</button>
-           <button>Everything is okay. I am safe. </button>
-           <button> I need help. Please contact me as soon as possible. </button>
-           <button> I am at the office </button>
-           <button> Emergency </button>
-           <button> I am at the park. </button>
+            
+            { messages.map((msg, index) => ( 
+                  <button type='button' key={index} onClick= {() => setMessage(msg)}> 
+                         {msg}
+                  </button>
+            ))}
+
+            <textarea placeholder='Type new message' value={message} onChange = {(ev) => setMessage(ev.target.value)}/>
+           
+           <button type='button' onclick={sendMessageHandeler}> Send </button>
        </div>
     );
 }
