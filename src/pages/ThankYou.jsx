9c7281f3dbd586f5479d227209ffa8e92de538b0
Munnisha <munnishav9@gmail.com>
2026-07-09 18:33:36 -0500

    Create basic Thank You page.

---
+++ src/pages/ThankYou.jsx
@@ -0,0 +1,16 @@
+
+
+function ThankYou(){
+
+    return(
+       
+        <div> 
+           <h3> Thank You for Registering! </h3>
+
+           <p> Your Account has been created. </p>
+
+         </div>
+    );
+}
+
+export default ThankYou;
