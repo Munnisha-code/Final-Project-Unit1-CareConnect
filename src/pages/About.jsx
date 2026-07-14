0584bcf57b759a8569eeb3aacd5590a64bd00fae
Munnisha <munnishav9@gmail.com>
2026-07-13 22:33:51 -0500

    Add About page card layout with Mission Vision grid, hover effects, and responsive styling.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,9 @@
 
 function About(){
     return(
-          <main>
-            <section>
+        <main className="about-container">
+
+            <section className="about-intro">
                 <h2>Introduction About the App</h2>
                 <p>
                   CareConnect is a Family Safety Application designed to help people stay connected with their loved ones and trusted contacts during daily activities, travel, and emergency situations. The main purpose of CareConnect is to provide a simple and reliable way for users to share their safety status, location, and important updates whenever support is needed.
@@ -16,21 +17,23 @@ function About(){
                 </p>
             </section>
 
-            <section>
+         <div className="mission-vision-container">
+
+            <section className="about-card">
                 <h2> Mission</h2>
                 <p>
                     Our mission is to improve personal safety through fast communication and easy-to-use technology. CareConnect focuses on creating a secure platform where users can manage trusted contacts, send emergency alerts, track previous updates, and stay connected with the people who care about them.
                 </p>
             </section>
 
-            <section>
+            <section className="about-card">
                  <h2> Vision</h2>
                  <p>
                    Our vision is to create a safer and more connected community where everyone can feel confident while traveling or performing daily activities. By providing simple safety features and quick communication tools, CareConnect aims to support users and their families whenever they need assistance.
                 </p>
             </section>
-
-            <section>
+         </div>
+            <section className="about-card">
                 <h2>Why You have to Choose Care Connect App</h2>
                 <p>
                     CareConnect is designed to make personal safety simple, fast, and reliable. 
@@ -54,7 +57,7 @@ function About(){
                 </p>
             </section>
 
-          </main>
+        </main>
 
     );
 }
