145d9b158eb6298e6fdc49d9187ec63b15310a55
Munnisha <munnishav9@gmail.com>
2026-07-14 22:20:57 -0500

    Styled footer component with responsive layout and contact links.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,10 @@ function Footer(){
               <p> @2026, All rights reserved to Care Connect Developer.</p>
 
               <p> Contact Me: { " "}
-              <a href ="munnisha_v@outlook.com"> Send Email</a> , 
-              <a href="https://www.linkedin.com/in/munnisha-v-07779a331/"> LinkedIn</a>
+                <a href ="mailto:munnisha_v@outlook.com"> Send Email </a> , 
+                <a href="https://www.linkedin.com/in/munnisha-v-07779a331/"> LinkedIn </a>
               </p>
-
-              
-              
-             
+  
         </footer>
  
 
