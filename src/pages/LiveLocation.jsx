397f69be65a86aeb6854988d81b309b07412290c
Munnisha <munnishav9@gmail.com>
2026-07-15 16:42:41 -0500

    Created LiveLocation page component with basic structure.

---
+++ src/pages/LiveLocation.jsx
@@ -0,0 +1,19 @@
+
+
+
+function LiveLocaion(){
+    return(
+
+        <main classname = 'live-location-container'>
+
+            <h2> Live Location </h2>
+
+            <p> Share your current location with your trusted contacts
+                during emergency situations. </p>
+                
+        </main>
+
+    );
+}
+
+export default LiveLocaion;
