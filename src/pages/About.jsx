fff4cf1dc0a6c907bc7fa0f8961e126b9b065824
Munnisha <munnishav9@gmail.com>
2026-07-15 15:14:17 -0500

    Added About page images gallery using map function with horizontal scrolling.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,45 @@
 
-
-
+import careConnectTogether from '../assets/images/careconnecttogether.jpg';
+import family from '../assets/images/family.jpg';
+import lovedOnes from '../assets/images/lovedones.jpg';
+import fatherLove from '../assets/images/fatherLove.jpg';
+import staySafe from '../assets/images/staySafe.jpg';
+import together from '../assets/images/together.jpg';
+import emergency from '../assets/images/emergency.jpg';
 
 
 function About(){
+     
+   const images = [careConnectTogether, family, lovedOnes, fatherLove, staySafe, together, emergency];
+
     return(
-        <main className="about-container">
+       
+       <main className="about-container">     
+
+            <section className = 'about-gallery'>
+
+                <h2> CareConnect features</h2>
+
+                <div className='image-container'>
+                    {images.map((image, index) => (
+                        <img src = {image}
+                             key = {index}
+                             alt = 'CareConnect Safety features'/>
+                    ))}
+
+                </div>
+
+            </section>    
 
             <section className="about-intro">
                 <h2>Introduction About the App</h2>
                 <p>
-                  CareConnect is a Family Safety Application designed to help people stay connected with their loved ones and trusted contacts during daily activities, travel, and emergency situations. The main purpose of CareConnect is to provide a simple and reliable way for users to share their safety status, location, and important updates whenever support is needed.
-                </p>
-                <p>  
+                  CareConnect is a Family Safety Application designed to help people stay connected with their loved ones and trusted contacts during daily activities, travel, and emergency situations. The main purpose of CareConnect is to provide a simple and reliable way for users to share their safety status, location, and important updates whenever support is needed.  
                     In today's busy world, many people travel alone for work, education, or personal activities. CareConnect helps reduce safety concerns by allowing users to quickly send updates such as reaching home safely, traveling, or requesting help. These updates are shared with selected trusted contacts so that family members and friends can stay informed about the user's situation.  
                 </p>
             </section>
+
+            
 
          <div className="mission-vision-container">
 
@@ -33,6 +57,7 @@ function About(){
                 </p>
             </section>
          </div>
+
             <section className="about-card">
                 <h2>Why You have to Choose Care Connect App</h2>
                 <p>
