3082781fc1820cf12e60dca282da2c375f2b6c4e
Munnisha <munnishav9@gmail.com>
2026-07-17 09:31:34 -0500

    Fixed confirm password validation error in register form.

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -53,7 +53,7 @@ function RegisterForm(){
                 <input type ='password' name = 'password' value = {password} onChange ={changeHandler} /> <br />
 
                 <label> Confirm Password: </label>
-                <input type ='confirmPassword' name = 'Password' value = {confirmPassword} onChange ={changeHandler} /> <br /> <br />
+                <input type ='confirmPassword' name = 'confirmPassword' value = {confirmPassword} onChange ={changeHandler} /> <br /> <br />
 
                 <button className='register-button' type = 'submit'> Register </button> <br /> <br />
 
