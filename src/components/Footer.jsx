87c6f1ad10666f284cbdf5b492ba2ac6e710ac14
Munnisha <munnishav9@gmail.com>
2026-07-17 15:42:45 -0500

    Updated footer styling and content.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,13 @@ function Footer(){
     return(
         
         <footer>
-              <h4> Built by : Munnisha Vissamsetti.</h4>
+              
               <p> @2026, All rights reserved to Care Connect Developer.</p>
 
-              <p> Contact Me: { " "}
-                <a href ="mailto:munnisha_v@outlook.com"> Send Email </a> , 
+              <p> Contact: { " "}
+
                 <a href="https://www.linkedin.com/in/munnisha-v-07779a331/"> LinkedIn </a>
+                
               </p>
   
         </footer>
